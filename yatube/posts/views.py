1f095676a8f52ef6c52413e15b8cce9006c61571
Даниил Паутов <danpautoff@yandex.ru>
2022-05-23 13:47:51 +0300

    Подкрутил дженерики для API.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,25 +1,17 @@
-from http import HTTPStatus
-from http.client import CREATED
-
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponseForbidden
 from django.shortcuts import (
-    get_list_or_404,
     get_object_or_404,
     redirect,
     render,
 )
 from django.urls import reverse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .factories import create_page_obj
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post, User
-from .serializers import PostSerializer
 
 
 def index(request):
--- yatube/posts/views_api.py
+++ yatube/posts/views_api.py
@@ -1,60 +1,21 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 
 from .models import Post
 from .serializers import PostSerializer
 
 
-class APIPostList(APIView):
+class APIPostList(ListCreateAPIView):
     """Создание нового объекта и чтение всех для модели Post."""
 
-    def get(self, request):
-        """Обработка GET запроса."""
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        """Обработка POST запроса."""
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
-class APIPostDetail(APIView):
+class APIPostDetail(RetrieveUpdateDestroyAPIView):
     """Чтение, изменение и удаление опеределенного объекта для модели Post."""
 
-    def get(self, request, pk):
-        """Обработка GET запроса."""
-        post = get_object_or_404(Post, pk=pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        """Обработка PUT запроса."""
-        post = get_object_or_404(Post, pk=pk)
-        serializers = PostSerializer(
-            instance=post, data=request.data, partial=True
-        )
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_200_OK)
-
-    def patch(self, request, pk):
-        """Обработка PATCH запроса."""
-        post = get_object_or_404(Post, pk=pk)
-        serializers = PostSerializer(
-            instance=post, data=request.data, partial=True
-        )
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_200_OK)
-
-    def delete(self, request, pk):
-        """Обработка DELETE запроса."""
-        get_object_or_404(Post, pk=pk).delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
