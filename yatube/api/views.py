460e0f1c0f5bc4225d54eec5f792da7c37192c05
Даниил Паутов <danpautoff@yandex.ru>
2022-05-27 12:44:45 +0300

    Небольшие исправления.

--- yatube/api/views.py
+++ yatube/api/views.py
@@ -1,10 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from posts.models import Post
 from .serializers import PostSerializer
 
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(ReadOnlyModelViewSet):
     """Реализация CRUD для модели Post."""
 
     queryset = Post.objects.all()
